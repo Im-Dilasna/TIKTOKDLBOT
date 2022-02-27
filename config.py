75f4fdf624f4a529795450c140283328ac2981aa
IamNimsara <100426232+IamNimsara@users.noreply.github.com>
2022-02-27 00:13:52 -0800

    🌷 Config 🌷

--- config.py
+++ config.py
@@ -29,6 +29,6 @@ import os
 class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
     
